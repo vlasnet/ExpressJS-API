cf489d378222b59341122e993c804a65e37e5097
Serhii Vlasiuk <svlasy@softserveinc.com>
2026-02-04 16:18:42 +0100

    Add JWT token in the login response

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,26 @@
 import 'reflect-metadata';
 import { inject, injectable } from 'inversify';
-import type { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { BaseController } from '../common/base.controller.js';
 import { ValidateMiddleware } from '../common/validate.middleware.js';
 import type { UserRegisterDto } from './dto/user-register.dto.js';
 import { UserRegisterDto as DtoUserRegister } from './dto/user-register.dto.js';
 import type { UserLoginDto } from './dto/user-login.dto.js';
 import { UserLoginDto as DtoUserLogin } from './dto/user-login.dto.js';
-import { UsersService } from './users.service.js';
-import { TYPES } from '../types.js';
 import { HTTPError } from '../errors/http-error.js';
+import { TYPES } from '../types.js';
+import type { NextFunction, Request, Response } from 'express';
 import type { ILogger } from '../logger/logger.interface.js';
 import type { IUserController } from './users.controller.interface.js';
+import type { IConfigService } from '../config/confige.service.interface';
+import type { IUserService } from './dto/users.service.interface';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.ILogger) private loggerService: ILogger,
-		@inject(TYPES.UserService) private userService: UsersService,
+		@inject(TYPES.UserService) private userService: IUserService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -43,7 +46,9 @@ export class UserController extends BaseController implements IUserController {
 	): Promise<void> {
 		const isSuccess = await this.userService.validateUser(body);
 		if (isSuccess) {
-			this.ok(res, {});
+			this.loggerService.log('Success login at ' + new Date().toISOString());
+			const jwt = await this.signJWT(body.email, this.configService.get('SECRET'));
+			this.ok(res, { jwt });
 		} else {
 			return next(new HTTPError(401, 'Not Authorized', 'login'));
 		}
@@ -61,5 +66,25 @@ export class UserController extends BaseController implements IUserController {
 		}
 
 		this.ok(res, { id: newUser.id, name: newUser.name, email: newUser.email });
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			jwt.sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{ algorithm: 'HS256' },
+				(err, token) => {
+					if (err) {
+						reject(err);
+					} else if (token) {
+						resolve(token);
+					}
+				},
+			);
+		});
 	}
 }
