f9e65dfc899833b563c933df215405abef3f5dca
Serhii Vlasiuk <svlasy@softserveinc.com>
2026-02-05 15:11:12 +0100

    Complete e2e tests and add coverage

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -3,7 +3,6 @@ import type { Config } from 'jest';
 const config: Config = {
 	verbose: true,
 	preset: 'ts-jest',
-	rootDir: './tests',
 	testRegex: '.e2e.spec.ts$',
 };
 
--- tests/users.e2e.spec.ts
+++ tests/users.e2e.spec.ts
@@ -2,6 +2,9 @@ import { App } from '../src/app';
 import { boot } from '../src/main';
 import request from 'supertest';
 
+//TODO: In the current implementation, the environmental issue is not resolved.
+// To run these test imports extension of many files should be without .js
+
 let application: App;
 
 beforeAll(async () => {
@@ -12,9 +15,49 @@ beforeAll(async () => {
 describe('Users e2e', () => {
 	it('Register - error', async () => {
 		const res = await request(application.app)
-			.post('users/register')
+			.post('/users/register')
 			.send({ email: 'test@gmail.com', password: 'qwerty' });
 
 		expect(res.statusCode).toBe(422);
 	});
+	it('Login - success', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'test@gmail.com', password: 'qwerty' });
+
+		expect(res.statusCode).toBe(200);
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+	it('Login - error', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'test@gmail.com', password: 'qwertyWrong' });
+
+		expect(res.statusCode).toBe(401);
+	});
+	it('Info - success', async () => {
+		const loginRes = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'test@gmail.com', password: 'qwerty' });
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${loginRes.body.jwt}`);
+
+		expect(res.statusCode).toBe(200);
+		expect(res.body).toEqual({ id: 1, name: 'test-user', email: 'test@gmail.com' });
+	});
+	it('Info - error', async () => {
+		const loginRes = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'test@gmail.com', password: 'qwerty' });
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${loginRes.body.jwt}WRONG`);
+
+		expect(res.statusCode).toBe(401);
+	});
+});
+
+afterAll(() => {
+	application.close();
 });
